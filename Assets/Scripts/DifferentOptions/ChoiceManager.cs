8e882dd43ee100beedef8a71632e2b4dba5b00bd
Tatum13 <tatumdv2001@gmail.com>
2023-11-07 15:43:48 +0100

    added new struct for the dialogue and player responses

---
+++ Assets/Scripts/DifferentOptions/ChoiceManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Choice : MonoBehaviour
+{
+    [SerializeField] private GameObject textBox;
+    [SerializeField] private string choicePlayer;
+
+    public void OnChoiceButtonOne()
+    {
+        
+    }
+    public void OnChoiceButtonTwo()
+    {
+        
+    }
+}
